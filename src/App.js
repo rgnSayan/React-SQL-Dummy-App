106da05af2012c450f2bc68c5d89d954a73051e1
rgnSayan <1805603@kiit.ac.in>
2022-05-29 22:24:58 +0530

    Loading Screen Added, Deployed in firebase and UI changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import ClockLoader from "react-spinners/ClockLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(!loading);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      {loading ? (
+        <div className="loader">
+          <ClockLoader color={"#56c9aa"} loading={loading} size={150} />
+        </div>
+      ) : (
+        <React.Fragment>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </React.Fragment>
+      )}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZST_YlWvdmZ_5IjW-D3mEn4Ze6iKA6Sk",
+  authDomain: "react-sql-query.firebaseapp.com",
+  projectId: "react-sql-query",
+  storageBucket: "react-sql-query.appspot.com",
+  messagingSenderId: "216795600246",
+  appId: "1:216795600246:web:a953832b34000eb4765657",
+};
